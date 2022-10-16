3dde56d7a9263a9b86f6c6d18d1678f4d173021b
letter333 <seryong12@gmail.com>
2022-10-17 00:47:05 +0900

    jsp 게시판 시작 20221017

--- jsp_self/StudyBasicJSP/oracleconn.sql
+++ jsp_self/StudyBasicJSP/oracleconn.sql
@@ -15,3 +15,34 @@ insert into member(id, password, name, reg_date) values ('kingdora@dragon.com', 
 insert into member(id, password, name, reg_date) values ('honkd@aaa.com', '1111', 'ȫ�浿', sysdate);
 select * from member;
 delete from member;
+
+create sequence board_num;
+delete sequence board_num;
+
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
+create table board(
+num number(10) not null primary key,
+writer varchar2(10) not null,
+email varchar2(30),
+subject varchar2(255) not null,
+password varchar2(16) not null,
+reg_date timestamp(0) not null,
+readcount number(10) default 0,
+ref number(10) not null,
+re_step number(5) not null,
+re_level number(5) not null,
+content clob not null,
+ip varchar2(20) not null
+);
+
+delete sequence board_seq;
+-- Generate ID using sequence and trigger
+create sequence board_seq start with 1 increment by 1;
+
+create or replace trigger board_seq_tr
+ before insert on board for each row
+ when (new.num is null)
+begin
+ select board_seq.nextval into :new.num from dual;
+end;
+/
